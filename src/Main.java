e0c59e0f1190eab0861783c6fb638e14344f0ff5
jkeating2025 <jkeating2025@cchsdons.com>
2024-03-21 16:33:04 -0700

    commit test

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,48 @@ public class Main {
     static ArrayList<Item> inventory = new ArrayList<>();
     public static void main(String[] args) {
         Scene strangeRoom1 = new Scene();
+        strangeRoom1.setStoryText("You wake up in a strange room in an unfamiliar house… You try the door but it's locked. Maybe there’s a key nearby…");
+        Choice
         //If scanner.lastword() = posChoice.getText.
-        //go to hallway.
+        //go to hallway
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         Scene hallway = new Scene();
         Key bluekey = new Key("Blue Key", "blue");
         inventory.add(bluekey);
