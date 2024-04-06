f36307c6815164d0a5446f4f556b9a036774f66f
CitraTune <tacticalyummnyness10@gmail.com>
2024-04-05 19:45:47 -0700

    4/5

--- src/Scene.java
+++ src/Scene.java
@@ -14,8 +14,8 @@ public class Scene {
     public void printScene(){
         System.out.println(storyText);
         System.out.println("You can:");
-        for(int i = 0; i < choices.size(); i++){
-            System.out.println(choices.get(i).getChoiceDescription());
+        for (Choice choice : choices) {
+            System.out.println(choice.getChoiceDescription());
         }
     }
     //Each Scene has multiple choices, and each choice can have multiple outcomes. This method, choice, outputs a scene to go to after a choice is picked.
@@ -23,8 +23,8 @@ public class Scene {
     //It can also do other things, like say "this.setRead = true;" which makes the game aware to do repeatText, and add items to the inventory and therefore to the capabilities
     public Scene choice(int userChoice){
         //Cycle through all choices to check if what the user typed matches an option
-        for(int i = 0; i< choices.size(); i++) {//Cycle through all of choices
-            Choice pickedChoice = choices.get(i);//Get current choice of cycle, call this pickedChoice
+        //Get current choice of cycle, call this pickedChoice
+        for (Choice pickedChoice : choices) {//Cycle through all of choices
             //However, pickedChoice doesn't really make sense as "picked" if it's not picked. This if statement makes sure it is picked.
             if (userChoice == Character.getNumericValue(pickedChoice.getChoiceDescription().charAt(0))) {
                 //If it does match, get the outcome and do the appropriate thing, like giving an item.
@@ -33,15 +33,13 @@ public class Scene {
                 //So, we will cycle through the capabilities array, and then compare that to the getOutcome class. If the outcome requirement string matches our capabilites, it happens.
                 //However, if the outcome requirement string doesn't match a capability, then we default to outcome index of 0.
                 Outcome pickedChoiceOutcome = pickedChoice.getOutcome(0);
-                String requirement = "";
 
-                int eventNumber;
                 //Cycle through all capabilities
-                for(int j = 0; j < Main.capabilities.size(); j++) {
+                for (int j = 0; j < Main.capabilities.size(); j++) {
                     //Cycle through all outcomes
                     for (int k = 0; k < pickedChoice.getOutcomes().size(); k++) {
                         //Cycle through all requirements for every outcome
-                        for (int l = 0; l < pickedChoice.getOutcomes().get(k).getRequirements().size(); l++){
+                        for (int l = 0; l < pickedChoice.getOutcomes().get(k).getRequirements().size(); l++) {
                             //This one's a bit of a doozy in terms of readability
                             //It means that we look at the pickedChoice, then look at the first outcome (k) and it's requirement (l).
                             //Then we cycle through the requirements of the first outcome, by using the for loop for l
@@ -76,10 +74,11 @@ public class Scene {
                     case "nextScene3" -> {
                         return nextScenes.get(2);
                     }
-                    case "ending" -> {
+                    case "ending" ->
                         System.out.println("Congratulations! You Escaped! Restart to play again!");
 
-                    }
+
+                    default -> throw new IllegalStateException("Unexpected value: " + pickedChoiceOutcome.getEvent());
                 }
             }
         }
